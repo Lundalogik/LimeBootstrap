eea0eca17fe8f9f90addff244c53c461fb58427c
Filip Persson <filip.persson@me.com>
2013-10-09 17:40:40 +0200

    Updated checklist. Can now add by yourself

--- apps/checklist/app.js
+++ apps/checklist/app.js
@@ -3,7 +3,7 @@ lbs.apploader.register('checklist', function () {
     //config
     self.config = {
         dataSources: [
-
+            {type: 'xml', source: 'Checklist.Initialize'}
         ],
         resources: {
             scripts: [],
@@ -11,82 +11,102 @@ lbs.apploader.register('checklist', function () {
             libs: ['json2xml.js']
         },
         name: 'Checklista',
-        canBeUnchecked: false,
-        canAddTask: false
+        canBeUnchecked: true,
+        canAddTask: true
     },
 
     //initialize
     self.initialize = function (node,appData) {
-        
+        function task() {
+            return {
+                idchecklist: "",
+                order: "",
+                title: "",
+                mouseover: "",
+                isChecked : ko.observable(false),
+                checkedDate : ko.observable(""),
+                checkedBy : ko.observable("")
+            }
+        }
+
+        self.registerHandlers();
+
         /**
         Checklistmodel
         */
-        var ChecklistModel = function(checklist){
+        function ChecklistModel(checklist) {
             var me = this;
 
             me.tasks = ko.observableArray();
             //populate tasks
             for (var i = 0; i < checklist.length; i++) {
-                //Is the task checked?
-                checklist[i].isChecked = ko.observable(checklist[i].isChecked);
                 //When and who checked?
-                if (checklist[i].checkedDate) {
-                    checklist[i].checkedDate = ko.observable(checklist[i].checkedDate);
-                    checklist[i].checkedBy = ko.observable(checklist[i].checkedBy);
-                }else{
-                    checklist[i].checkedDate = ko.observable("");
-                    checklist[i].checkedBy = ko.observable("");
-                }; 
-                me.tasks.push(checklist[i])
+                var tempTask = task();
+                tempTask.idchecklist = checklist[i].idchecklist;
+                tempTask.order = checklist[i].order;
+                tempTask.title = checklist[i].title;
+                tempTask.mouseover = checklist[i].mouseover;
+                if (checklist[i].isChecked === "true") {
+                    tempTask.isChecked(true);
+                    tempTask.checkedDate(checklist[i].checkedDate);
+                    tempTask.checkedBy(checklist[i].checkedBy);
+                }
+                me.tasks.push(tempTask);
             };
-
             //name
             me.name = self.config.name;
             me.canAddTask = self.config.canAddTask;
             //Nbr of checkedItems
-            me.nbrOfChecked =   ko.computed(function(){
+            me.nbrOfChecked = ko.computed(function(){
                 return ko.utils.arrayFilter(me.tasks(), function(task) {
                         return task.isChecked() == true;
                  }).length;
              });
+            me.inputValue = ko.observable('');
+            me.isSelected = ko.observable(false)
 
             //click event
             me.taskClicked = function(task){
                 try{
-                    if(!task.isChecked() && task.idchecklist){
-                        task.isChecked (lbs.common.executeVba("Checklist.PerfromAction," + task.idchecklist));
-                        task.checkedDate(moment());
+                    if(!task.isChecked()){
+                        task.checkedDate(moment().toISOString());
                         task.checkedBy(lbs.limeDataConnection.ActiveUser.Name);
-                        me.save();
-
+                        if(task.idchecklist){
+                            task.isChecked (lbs.common.executeVba("Checklist.PerfromAction," + task.idchecklist));
+                        }else{
+                            task.isChecked(true);
+                        }
                     }else{
                         if(self.config.canBeUnchecked){
                             task.isChecked(false);
+                            task.checkedDate("");
+                            task.checkedBy("");
                         }
                     }
+                    me.save();
                 }catch(e){
                     task.isChecked(false);  
                 }
             }
-
+            //Create and add new Task
             me.addTask = function(){
-                me.tasks.push({              
-                    "idchecklist": "1001",
-                    "order": "1",
-                    "title": "Beställ diarienummer",
-                    "mouseover": "Kontakta diariet och beställ diarienummer med rubrik Avtal om tillträde järnvägsföretag",
-                    "isChecked":true
-                });    
+                newTask = task();
+                if(me.inputValue){
+                    newTask.title = me.inputValue().trim(); 
+                    newTask.order = me.tasks().length + 1; 
+                
+                    me.tasks.push(newTask);
+                    me.inputValue(''); 
+                    me.save();                   
+                }
             }
-
+            //Save change to LIME
             me.save = function(){
                         var tempJSON = JSON.stringify({checklist:ko.toJS(me.tasks)});
-                        var tempXML = json2xml($.parseJSON(tempJSON),'');
+                        var tempXML = "<xmlchecklist>" + json2xml($.parseJSON(tempJSON),'') + "</xmlchecklist>";
                         lbs.common.executeVba("Checklist.Save," + tempXML) ;
-
             }
         }
-
 
         /**
         Dummy data
@@ -111,13 +131,40 @@ lbs.apploader.register('checklist', function () {
                     "title": "Skicka avtal och Excelfil",
                     "mouseover": "Skicka avtal samt Excelfil för kontaktuppgifter till företagets kontaktperson"
                   }
-                ]      
+                ]
             }
 
         /**
         Return view model
         */
-        return new ChecklistModel(dummyData.checklist);
+
+            return new ChecklistModel(appData.xmlchecklist.checklist);
+
+        
+    },
+
+    self.registerHandlers = function(){
+        var ENTER_KEY = 13;
+    // a custom binding to handle the enter key (could go in a separate library)
+        ko.bindingHandlers.enterKey = {
+            init: function (element, valueAccessor, allBindingsAccessor, data) {
+                var wrappedHandler, newValueAccessor;
+                // wrap the handler with a check for the enter key
+                wrappedHandler = function (data, event) {
+                    if (event.keyCode === ENTER_KEY) {
+                        valueAccessor().call(this, data, event);
+                    }
+                };
+                // create a valueAccessor with the options that we would want to pass to the event binding
+                newValueAccessor = function () {
+                    return {
+                        keyup: wrappedHandler
+                    };
+                };
+                // call the real event binding's init function
+                ko.bindingHandlers.event.init(element, newValueAccessor, allBindingsAccessor, data);
+            }
+        };
     }
 });
 
