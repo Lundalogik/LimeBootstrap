fc804b6f4608229d6b80c8e87b872b7530d46b09
Filip Persson <filip.persson@me.com>
2013-11-21 19:29:05 +0100

    Updates to creditinfo-app

--- apps/creditinfo/app.js
+++ apps/creditinfo/app.js
@@ -5,7 +5,7 @@ lbs.apploader.register('creditinfo', function () {
     this.config = {
         customernbr : "5563970465",
         dataSources: [
-           
+     
         ],
         resources: {
             scripts: [],
@@ -22,15 +22,44 @@ lbs.apploader.register('creditinfo', function () {
         
         viewModel.ratingValue = ko.observable();
         viewModel.ratingText = ko.observable();
+        viewModel.refreshEnabled = ko.observable(false);
+
+        viewModel.showGetIcon = function() {
+            viewModel.refreshEnabled(true);
+        }
+
+        viewModel.hideGetIcon = function() {
+            viewModel.refreshEnabled(false);
+        }
 
         viewModel.getRating = function(){
             var ratingData = {};
             ratingData = lbs.loader.loadDataSources(ratingData, [{type: 'xml', source: 'CreditInfo.GetCompanyRating,556397-0465', alias:'creditdata'}], true);
+
             ratingData = ratingData.creditdata.DataImport2Result.Blocks.Block.Fields.Field
             
             viewModel.ratingValue(ratingData[0].Value)
             viewModel.ratingText(ratingData[1].Value)
         }
+
+        viewModel.ratingColor = ko.computed( function(){
+           if (viewModel.ratingValue()){
+                if (viewModel.ratingValue() >= 8 )  { 
+                    return "good annimation"
+                } 
+                else if (viewModel.ratingValue() <= 7 &&  viewModel.ratingValue() >= 4){
+                    return "medium annimation" 
+                }
+                else if (viewModel.ratingValue() <= 3 &&  viewModel.ratingValue() >= 0){
+                    return "bad annimation" 
+                }
+
+           }else{
+                return ""
+           }
+        });
+
+
 
         return viewModel;
     }
