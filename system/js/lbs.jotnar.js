70f447a08cc13f0bc8249ecf42dc22c0e93baf77
Kahrstrom <jonatan.kahrstrom@gmail.com>
2015-10-30 08:50:29 +0100

    No more snow for reals

--- system/js/lbs.jotnar.js
+++ system/js/lbs.jotnar.js
@@ -2,19 +2,7 @@ lbs.jotnar = {
 
 	winterEgg : function(){
 		
-	    var n = moment();
-	    var start = "2015-12-19";
-	    var stop = "2016-01-05";
-	    
-	    var diffStart = moment(n).diff(moment(start,"YYYY-MM-DD")); 
-	    var diffStop = moment(n).diff(moment(stop,"YYYY-MM-DD"));
-
-	    if(diffStart > 0 && diffStop < 0 ){
-	    	snowStorm.freezeOnBlur = false;
-	    }
-	    else{
-	    	snowStorm.freeze();
-	    }
+	    //TBI
 		
 	}
 };
