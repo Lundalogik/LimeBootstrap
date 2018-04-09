b3225ad0ac5ad720822f7a3bf40f434cdbca75de
Filip Arenbo <filip@arenbo.org>
2018-04-09 23:13:03 +0200

    First attac on datasources

---
+++ src/js/dataSources/lbs.dataSource.js
@@ -0,0 +1,11 @@
+export default class DataSource {
+    constructor({ type, source, alias = '' }) {
+        this.type = type
+        this.source = source
+        this.alias = alias
+    }
+
+    static fetch() {
+        throw { name: 'NotImplementedError', message: 'Should be implemented by subclass' } 
+    }
+}
---
+++ src/js/dataSources/lbs.dataSource.limeobject.js
@@ -0,0 +1,16 @@
+import dataSource from './lbs.dataSource'
+
+export default class LimeObject extends dataSource {
+    constructor({ id, limetype, ...rest }) {
+        super(rest)
+        this.id = id
+        this.limetype = limetype
+    }
+
+    async fetch() {
+        const response = await fetch(`/api/${this.limetype}/${this.id}/`, {
+            headers: { 'x-session': lbs.session },
+        })
+        return response
+    }
+}
