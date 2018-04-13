90032534778eeb074ed57ccd1569b6ebed67c0fa
Jonatan Tegen <Jonatan.Tegen@lime.tech>
2018-04-13 16:15:29 +0200

    Added component lime-button

---
+++ src/components/lime-button/lime-button.js
@@ -0,0 +1,34 @@
+import ko from 'knockout'
+import limeButtonTemplate from './lime-button.tpl.html'
+import Log from '../../lib/lbs.log'
+
+class LimeButtonVM {
+    constructor(params) {
+        const {
+            bootstrapClass = '',
+            color = '',
+            text = '',
+            icon = '',
+            centered = true,
+        } = params
+        this.text = text
+        this.icon = icon
+        this.bootstrapClass = bootstrapClass
+        this.centered = centered
+        this.color = color
+        this.logger = new Log()
+        this.cssClasses = [this.bootstrapClass, this.color]
+
+        if (this.color && this.bootstrapClass) {
+            this.logger.warn(
+                `Cannot combine css classes '${this.bootstrapClass}' and '${this.color}'`)
+        }
+        if (this.centered) {
+            this.cssClasses.push('centered')
+        }
+
+        this.css = this.cssClasses.join(' ')
+    }
+}
+
+ko.components.register('lime-button', { viewModel: LimeButtonVM, template: limeButtonTemplate })
