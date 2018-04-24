57a25a109c4a4841e5f4f9cc3dc158ba54a959ae
Filip Arenbo <filip@arenbo.org>
2018-04-24 21:20:59 +0200

    added test for session

--- __tests__/lbs.test.js
+++ __tests__/lbs.test.js
@@ -6,7 +6,7 @@ lbs.externalConfig = config
 beforeAll(() => {
     window.lbs = lbs
     // see issue https://github.com/facebook/jest/issues/5124 for window.location
-    window.history.pushState({}, '', '?id=1001#company')
+    window.history.pushState({}, '', '?id=1001&session=123-abc-567#company')
     lbs.setup()
 })
 
@@ -17,3 +17,8 @@ test('A URL hash should be parsed and set activeClass', () => {
 test('URL parameters for id should be parsed', () => {
     expect(lbs.activeInspectorId).toEqual(1001)
 })
+
+test('URL parameters for id should be parsed', () => {
+    expect(lbs.session).toEqual('123-abc-567')
+})
+
