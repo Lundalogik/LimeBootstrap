0507032f99c7307bd00110cad7261c68d2b1f51c
Filip Arenbo <filip@arenbo.org>
2018-06-01 13:25:02 +0200

    Added a mock response

---
+++ __mocks__/api_response_lime_core_company_related_persons.js
@@ -0,0 +1,227 @@
+module.exports = {
+    _embedded: {
+        limeobjects: [
+            {
+                mobilephone: '+4532449812',
+                firstname: 'Andreas',
+                lastname: 'Mortensen',
+                _createdtime: '2016-09-15T09:15:54.733000+02:00',
+                position: 'Försäljningsansvarig',
+                name: 'Andreas Mortensen',
+                _links: {
+                    new_helpdesk: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1006/helpdesk/new/',
+                    },
+                    relation_history: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1006/history/',
+                        name: 'history',
+                    },
+                    relation_todo: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1006/todo/',
+                        name: 'todo',
+                    },
+                    new_deal: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1006/deal/new/',
+                    },
+                    limetype: {
+                        href: 'https://prostaging/core54/api/v1/limetype/person/',
+                        name: 'person',
+                    },
+                    new_document: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1006/document/new/',
+                    },
+                    new_todo: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1006/todo/new/',
+                    },
+                    self: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1006/',
+                    },
+                    new_participant: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1006/participant/new/',
+                    },
+                    relation_deal: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1006/deal/',
+                        name: 'deal',
+                    },
+                    new_history: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1006/history/new/',
+                    },
+                    relation_helpdesk: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1006/helpdesk/',
+                        name: 'helpdesk',
+                    },
+                    relation_company: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1006/company/',
+                        name: 'company',
+                    },
+                    relation_participant: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1006/participant/',
+                        name: 'participant',
+                    },
+                    relation_document: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1006/document/',
+                        name: 'document',
+                    },
+                },
+                email: 'andreas.mortensen@limedemo.com',
+                _createduser: 1,
+                _id: 1006,
+                inactive: false,
+                _descriptive: 'Andreas Mortensen',
+                _timestamp: '2017-01-27T11:58:03.127000+01:00',
+                _updateduser: 2901,
+                phone: '+4586234533',
+                company: 1011,
+            },
+            {
+                mobilephone: '+4534226545',
+                firstname: 'Lene',
+                lastname: 'Hansen',
+                _createdtime: '2016-09-15T09:15:59.193000+02:00',
+                position: 'IT-chef',
+                name: 'Lene Hansen',
+                _links: {
+                    new_helpdesk: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1042/helpdesk/new/',
+                    },
+                    relation_history: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1042/history/',
+                        name: 'history',
+                    },
+                    relation_todo: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1042/todo/',
+                        name: 'todo',
+                    },
+                    new_deal: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1042/deal/new/',
+                    },
+                    limetype: {
+                        href: 'https://prostaging/core54/api/v1/limetype/person/',
+                        name: 'person',
+                    },
+                    new_document: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1042/document/new/',
+                    },
+                    new_todo: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1042/todo/new/',
+                    },
+                    self: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1042/',
+                    },
+                    new_participant: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1042/participant/new/',
+                    },
+                    relation_deal: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1042/deal/',
+                        name: 'deal',
+                    },
+                    new_history: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1042/history/new/',
+                    },
+                    relation_helpdesk: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1042/helpdesk/',
+                        name: 'helpdesk',
+                    },
+                    relation_company: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1042/company/',
+                        name: 'company',
+                    },
+                    relation_participant: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1042/participant/',
+                        name: 'participant',
+                    },
+                    relation_document: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1042/document/',
+                        name: 'document',
+                    },
+                },
+                email: 'lene.hansen@limedemo.com',
+                _createduser: 1,
+                _id: 1042,
+                inactive: false,
+                _descriptive: 'Lene Hansen',
+                _timestamp: '2017-01-27T13:16:34.127000+01:00',
+                _updateduser: 2901,
+                phone: '+4586234321',
+                company: 1011,
+            },
+            {
+                mobilephone: '+4520234545',
+                firstname: 'Peter',
+                lastname: 'Andersen',
+                _createdtime: '2016-09-15T09:16:02.227000+02:00',
+                position: 'VD',
+                name: 'Peter Andersen',
+                _links: {
+                    new_helpdesk: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1055/helpdesk/new/',
+                    },
+                    relation_history: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1055/history/',
+                        name: 'history',
+                    },
+                    relation_todo: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1055/todo/',
+                        name: 'todo',
+                    },
+                    new_deal: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1055/deal/new/',
+                    },
+                    limetype: {
+                        href: 'https://prostaging/core54/api/v1/limetype/person/',
+                        name: 'person',
+                    },
+                    new_document: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1055/document/new/',
+                    },
+                    new_todo: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1055/todo/new/',
+                    },
+                    self: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1055/',
+                    },
+                    new_participant: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1055/participant/new/',
+                    },
+                    relation_deal: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1055/deal/',
+                        name: 'deal',
+                    },
+                    new_history: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1055/history/new/',
+                    },
+                    relation_helpdesk: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1055/helpdesk/',
+                        name: 'helpdesk',
+                    },
+                    relation_company: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1055/company/',
+                        name: 'company',
+                    },
+                    relation_participant: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1055/participant/',
+                        name: 'participant',
+                    },
+                    relation_document: {
+                        href: 'https://prostaging/core54/api/v1/limeobject/person/1055/document/',
+                        name: 'document',
+                    },
+                },
+                email: 'peter.andersen@limedemo.com',
+                _createduser: 1,
+                _id: 1055,
+                inactive: false,
+                _descriptive: 'Peter Andersen',
+                _timestamp: '2016-09-15T11:08:07.033000+02:00',
+                _updateduser: 1,
+                phone: '+4586230001',
+                company: 1011,
+            },
+        ],
+    },
+    _links: {
+        self: {
+            href: 'https://prostaging/core54/api/v1/limeobject/company/1011/person/?_limit=10',
+        },
+    },
+}
