f0a90118496dc669599c92dfcb586933580b1f8f
Jonatan Tegen <Jonatan.Tegen@lime.tech>
2018-06-10 12:42:37 +0200

    Changed appearance for lbs-loading-indicator and added sizing

--- src/components/lbs-loading-indicator/lbs-loading-indicator.js
+++ src/components/lbs-loading-indicator/lbs-loading-indicator.js
@@ -2,8 +2,27 @@ import ko from 'knockout'
 import lbsLoadingIndicatorTemplate from './lbs-loading-indicator.tpl.html'
 
 export default class LoadingIndicator {
-    constructor(params) {
-        this.loading = params.loading
+    constructor({ loading, size = 'md' }) {
+        this.loading = loading
+        const validSizes = ['sm', 'md', 'lg', 'xl']
+
+        switch (size) {
+        case 'sm':
+            this.size = ''
+            break
+        case 'md':
+            this.size = 'fa-lg'
+            break
+        case 'lg':
+            this.size = 'fa-2x'
+            break
+        case 'xl':
+            this.size = 'fa-3x'
+            break
+        default:
+            lbs.log.warn(`Loader size must be on of ${validSizes.join(', ')}`)
+            this.size = 'fa-lg'
+        }
     }
 }
 
