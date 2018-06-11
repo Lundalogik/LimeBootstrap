8b01dda2e5449895274ca2adfc1c497ee8015376
Filip Arenbo <filip@arenbo.org>
2018-06-11 22:24:03 +0200

    Catches failed loading of components

--- src/lib/lbs.componentLoader.js
+++ src/lib/lbs.componentLoader.js
@@ -6,13 +6,15 @@ export default class ComponentLoader {
     static async loadComponents(globalComponents, localComponents) {
         const components = _.union(localComponents, globalComponents)
         await Promise.all(components.map(async (component) => {
-            await $.getScript(component.path).done((script, status) => {
-                const r = require // hack to fool Brunch.io to avoid require at compile time
-                lbs.log.info(`Registering component ${component.name}`)
-                ko.components.register(component.name, { viewModel: r(`components/${component.name}/viewmodel.js`).default, template: r(`components/${component.name}/template.html`) })
-            }).fail((jqxhr, settings, exception) => {
-                lbs.log.error("Something went wrong"+exception )
-            })
+            try {
+                await $.getScript(component.path).done((script, status) => {
+                    const r = require // hack to fool Brunch.io to avoid require at compile time
+                    lbs.log.info(`Registering component ${component.name}`)
+                    ko.components.register(component.name, { viewModel: r(`components/${component.name}/viewmodel.js`).default, template: r(`components/${component.name}/template.html`) })
+                })
+            } catch (err) {
+                lbs.log.error(`Failed to load component '${component.name}'`, err)
+            }
         }))
     }
 }
