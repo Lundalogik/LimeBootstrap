3fac908687ad9037a87e1499233f3519e7a12116
Filip Arenbo <filip@arenbo.org>
2018-06-11 22:52:12 +0200

    Added css loading to component loader

--- src/lib/lbs.componentLoader.js
+++ src/lib/lbs.componentLoader.js
@@ -7,7 +7,8 @@ export default class ComponentLoader {
         const components = _.union(localComponents, globalComponents)
         await Promise.all(components.map(async (component) => {
             try {
-                await $.getScript(component.path).done((script, status) => {
+                $('<link/>', { rel: 'stylesheet', type: 'text/css', href: `${component.path}/component.css` }).appendTo('head')
+                await $.getScript(`${component.path}/component.js`).done(() => {
                     const r = require // hack to fool Brunch.io to avoid require at compile time
                     lbs.log.info(`Registering component ${component.name}`)
                     ko.components.register(component.name, { viewModel: r(`components/${component.name}/viewmodel.js`).default, template: r(`components/${component.name}/template.html`) })
