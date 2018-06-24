6cad67bd259ffbb221e84503817a086e3d35571c
Filip Arenbo <filip@arenbo.org>
2018-06-24 22:12:48 +0200

    get actionpad name from lbs instead

--- src/components/lbs-menu/lbs-menu-legacy.js
+++ src/components/lbs-menu/lbs-menu-legacy.js
@@ -1,13 +1,10 @@
 import $ from 'jquery'
 
-
-const ap = decodeURI((RegExp('ap=(.+?)(&|$)').exec(window.location.search) || [null, null])[1])
-
 const hideshow = (menu) => {
     const menuDiv = $(menu)
     $(menu).find('i').first().toggleClass('fa fa-angle-down') // expanded
     $(menu).find('i').first().toggleClass('fa fa-angle-right') // Hidden
-    if (lbs.bakery.getCookie(`${$(menu).index()}ul${ap}`) === '0') {
+    if (lbs.bakery.getCookie(`${$(menu).index()}ul${lbs.activeClass}`) === '0') {
         menuDiv.removeClass('collapsed')
         menuDiv.children('li').not('.remainHidden').fadeIn(200)
     } else {
@@ -21,7 +18,7 @@ $('#content').bind('load.complete', () => {
     $('.expandable').each((index, element) => {
         console.warn('[Deprecation] You are using the legacy menu markup. Update to the new component <lbs-menu>')
         const menuHeader = $(element).find('.menu-header')
-        if (lbs.bakery.getCookie(`${$(element).index()}ul${ap}`) === '0') {
+        if (lbs.bakery.getCookie(`${$(element).index()}ul${lbs.activeClass}`) === '0') {
             menuHeader.prepend("<i class='fa fa-angle-down'> </i>")
             $(element).removeClass('collapsed')
             $(element).children('li').not('.remainHidden').show()
@@ -36,10 +33,10 @@ $('#content').bind('load.complete', () => {
     $('.expandable').find('.menu-header').click((event) => {
         const element = event.currentTarget
         const menuDiv = $(element).parent()
-        let i = lbs.bakery.getCookie(`${menuDiv.index()}ul${ap}`)
+        let i = lbs.bakery.getCookie(`${menuDiv.index()}ul${lbs.activeClass}`)
 
         i = i === '0' ? '1' : '0'
-        lbs.bakery.setCookie(`${menuDiv.index()}ul${ap}`, i, '200')
+        lbs.bakery.setCookie(`${menuDiv.index()}ul${lbs.activeClass}`, i, '200')
         hideshow(menuDiv)
     })
 })
