ce0a1b054bf9d30f61f038c740666a514161f624
Filip Arenbo <filip@arenbo.org>
2018-06-26 09:59:05 +0200

    Renamed null check prop

--- src/components/lbs-guard/lbs-guard.js
+++ src/components/lbs-guard/lbs-guard.js
@@ -2,13 +2,13 @@ import ko from 'knockout'
 import lbsLoadingIndicatorTemplate from './lbs-guard.tpl.html'
 
 export default class Guard {
-    constructor({ activeUserIsMemberInOneOfGroups, isNotNull }) {
-        if (typeof activeUserIsMemberInOneOfGroups !== 'undefined' && typeof isNotNull !== 'undefined') {
-            this.shouldRenderContent = lbs.activeUser.isMemberInOneOf(activeUserIsMemberInOneOfGroups) && isNotNull
+    constructor({ activeUserIsMemberInOneOfGroups, exists }) {
+        if (typeof activeUserIsMemberInOneOfGroups !== 'undefined' && typeof exists !== 'undefined') {
+            this.shouldRenderContent = lbs.activeUser.isMemberInOneOf(activeUserIsMemberInOneOfGroups) && exists
         } else if (typeof activeUserIsMemberInOneOfGroups !== 'undefined') {
             this.shouldRenderContent = lbs.activeUser.isMemberInOneOf(activeUserIsMemberInOneOfGroups)
-        } else if (typeof isNotNull !== 'undefined') {
-            this.shouldRenderContent = Boolean(isNotNull)
+        } else if (typeof exists !== 'undefined') {
+            this.shouldRenderContent = Boolean(exists)
         }
     }
 }
