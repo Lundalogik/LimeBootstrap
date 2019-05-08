661e0a10876db012e15f6f4711c5a51f81a2443f
Filip Arenbo <filip@arenbo.org>
2019-05-08 20:50:32 +0200

    Spelling

--- src/components/lbs-hero/info-header-legacy.js
+++ src/components/lbs-hero/info-header-legacy.js
@@ -3,7 +3,7 @@ import $ from 'jquery'
 
 $('#content').bind('load.view', () => {
     $('.header-icon').each((index, element) => {
-        console.warn('[Deprication] You are using the legacy info-header markup. Update to the new component <info-header>')
+        console.warn('[Deprecation] You are using the legacy info-header markup. Update to the new component <info-header>')
         $(element).addClass('header-icon-container')
         $(element).css('background-image', `url('resources/${lbs.activeView}.png')`)
         $(element).append(`<img src="resources/${lbs.activeView}.png" class="header-icon-invis" />`)
